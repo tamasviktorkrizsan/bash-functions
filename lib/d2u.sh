3f90bb69904955a1ea8890eb6900d9ce951ef063
Tamás Krizsán <tamas@krizsanmunka.hu>
2021-06-02 18:27:49 +0200

    feat: add the "d2u.sh" function

---
+++ lib/d2u.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This file contain the d2u function.
+# Copyright (C) 2020 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### FUNCTION
+
+####################################################################
+# Convert line endings in text files to LF (linux compatible).
+# Arguments:
+#   input file
+####################################################################
+function d2u () {
+
+
+# INPUT PARAMETERS
+
+declare usr_input=${1:-*.sh};
+
+
+# OUTPUT
+
+for i in $usr_input;
+do dos2unix $i;
+echo "EXIT CODE:" $?;
+done;
+
+}
