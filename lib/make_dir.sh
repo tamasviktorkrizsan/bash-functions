9d2c3a854155ed58f4c0c4a76771f11f88d63327
Tamás Krizsán <tamas@krizsanmunka.hu>
2021-12-18 18:31:48 +0100

    refactor: change "$input" to "$usr_input"

--- lib/make_dir.sh
+++ lib/make_dir.sh
@@ -10,7 +10,7 @@
 ####################################################################
 # Create directory with custom options.
 # Arguments:
-#   new directory name
+#   new directory or path name
 ####################################################################
 function make_dir() {
 
@@ -25,25 +25,16 @@ declare usr_input="${1:-OUTPUT}";
 declare -r MKDIR_CONFIG="--verbose --parents --mode=777";
 
 
-### PROCESSING
-
-declare input;
-
-input=$(replace_whitespace "$usr_input");
-
-declare log_suffix="make_dir_$input.log";
-
-
 ### OUTPUT
 
 # TODO(tamasviktorkrizsan): temporarily disabled tee functionality to make
 # the "make_dir" function work properly
 
-if [[ -e $input ]];
+if [[ -e $usr_input ]];
 
-  then >&2 echo "The folder named < $input > is already exist.";
+  then >&2 echo "The folder named < $usr_input > is already exist.";
 
-  else mkdir $MKDIR_CONFIG "$input" >&2; # | tee "$input/$log_suffix";
+else mkdir $MKDIR_CONFIG "$usr_input"; # >&2 | tee "$usr_input/$log_suffix";
 
 fi
 
