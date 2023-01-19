120c947ca0aef905c74dc39613351659f40d3443
Tamás Krizsan <tamas@krizsanmunka.hu>
2023-01-19 13:36:27 +0100

    feature: add interactive bash header

---
+++ lib/header.interactive_bash.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Bash basic settings and functions for use in interactive shells.
+# source this file in ".bash_profile" to use.
+# Copyright (C) 2021 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+### BASH SETTINGS
+
+set -x;
+
+shopt -s nullglob;
+
+echo "LOAD INTERACTIVE SHELL FUNCTIONS";
+
+
+
+### INCLUDE FUNCTIONS
+
+
+source dev_tools.sh
+
+source d2u.sh
+
+source regex.sh
+
+source whitespace.sh
+
+source csv2json.sh
+
+source json.sh
+
+source io.sh
