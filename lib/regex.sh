0c29888b5434273656c9633fdce6490444193d79
Tamás Krizsan <tamas@krizsanmunka.hu>
2023-01-21 19:06:47 +0100

    feature: add "contains_pattern" function, add optional switch param

--- lib/regex.sh
+++ lib/regex.sh
@@ -6,27 +6,29 @@
 
 
 ################################################################################
-# Check if the input string contains the given literal pattern or not.
+# Check if the input string contains the given literal string or not.
+# You can reverse it ("NOT_contains") with the 3rd optional parameter value: "-v"
 # Arguments:
-#  string, pattern string (like: *)
+#  string, pattern (literal string, like: *)
 # Outputs:
 #  Boolean value
 ################################################################################
 function contains_literal () {
 
-
 ### INPUT PARAMETERS
 
 declare usr_input="${1}";
 
-declare usr_pattern="${2}"
+declare usr_pattern="${2}";
+
+declare switch="${3}";
 
 
 ### PROCESSING
 
 declare number_of_hits;
 
-number_of_hits=$(echo "$usr_input" | fgrep -c "$2");
+number_of_hits=$(echo "$usr_input" | fgrep $switch -c "$usr_pattern");
 
 
 ### OUTPUT
@@ -38,5 +40,43 @@ if ((0 < number_of_hits));
   else echo "false";
 
 fi
+
+}
+
+################################################################################
+# Check if the input string contains the given pattern or not.
+# You can reverse it ("NOT_contains") with the 3rd optional parameter value: "-v"
+# Arguments:
+#  string, pattern string, switch (optional)
+# Outputs:
+#  Boolean value
+################################################################################
+function contains_pattern () {
+
+  ### INPUT PARAMETERS
+
+  declare usr_input="${1}";
+
+  declare usr_pattern="${2}"
+
+  declare switch="${3}"
+
+
+  ### PROCESSING
+
+  declare number_of_hits;
+
+  number_of_hits=$(echo "$usr_input" | egrep $switch -c "$usr_pattern");
+
+
+  ### OUTPUT
+
+  if ((0 < number_of_hits));
+
+    then echo "true";
+
+    else echo "false";
+
+  fi
 
 }
