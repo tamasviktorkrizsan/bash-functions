3a5434dc3956406901069cc503d3de597cebff9c
Tamás Krizsan <tamas@krizsanmunka.hu>
2023-01-21 18:15:57 +0100

    feature: the "rename_whitespace" functions merged into 1 function

--- lib/whitespace.sh
+++ lib/whitespace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This file contains whitespace replacing functions.
-# Copyright (C) 2020 Tamas Viktor Krizsan
+# Copyright (C) 2020-2023 Tamas Viktor Krizsan
 # <https://github.com/tamasviktorkrizsan>
 # License: GPL-3.0-or-later
 
@@ -18,64 +18,67 @@ function rename_whitespace_string() {
 
   ## INPUT PARAMETERS
 
-  declare usr_input="${1-*}";
+  declare usr_input="${1}";
 
 
   ## PROCESSING
 
-  echo "$usr_input" | tr ' |' '_--';
+  echo "$usr_input" | tr ' ' '_';
 
   }
 
 
 ################################################################################
 # Remove whitespace and other OS prohibited characters from filenames or string
+# The behaviour depends on the enviroment where this function is called from
+#
 # Arguments:
-#   wildcard - DEFAULT (*) or wildcard extension (*.mkv)
+# in interactive shell - exact filename or wildcard pattern
+# in shell script - input string
+#
 # Outputs:
-#  renamed files. Whitespace (" ") replaced with underscores ("_")
+# Whitespaces (" ") replaced with underscores ("_")
+# in interactive shell - rename input filename(s)
+# in shell script - rename input string
 ################################################################################
 function rename_whitespace() {
 
 
 ## INPUT PARAMETERS
 
-declare usr_input="${1:-*}";
+declare usr_input="${1}";
 
-
-## CONSTANT SETTINGS
-
-IFS='\n'
-
-
-## VARIABLE SETTINGS
 
 declare string_fix;
 
 
-## PROCESSING
-
-if [[ -e $usr_input ]] || [[ $usr_input == '*' ]]; then
-
-  echo "INPUT DETECTED AS A FILENAME";
-
-  for i in $usr_input;
-
-    do string_fix=$(rename_whitespace_string "$i");
-
-    mv "$i" "$string_fix";
-
-  done;
+if [ -n "$PS1" ]; then
 
 
-else
+  # call in interactive shell
 
-  echo "INPUT DETECTED AS A INPUT STRING";
+  if $(contains_literal "$usr_input" "*"); then
 
-  string_fix=$(rename_whitespace_string "$usr_input");
+    for i in $usr_input; do
 
-  echo "$string_fix";
+      string_fix=$(rename_whitespace_string "$i");
 
-fi
+      mv "$i" "$string_fix";
+
+    done;
+
+
+  else string_fix=$(rename_whitespace_string "$usr_input");
+
+  fi;
+
+
+  # call in shell script
+
+  else string_fix=$(rename_whitespace_string "$usr_input");
+
+    echo "$string_fix";
+
+fi;
 
 }
